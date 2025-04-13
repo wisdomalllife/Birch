255e441e940407589da7d1c086ca78823dd3babd
wisdomalllife <55911109+wisdomalllife@users.noreply.github.com>
2025-04-13 15:55:36 +0300

    Update busco_stats.sh

--- non-Karelian_denovo_assembly/quality_check/busco_stats.sh
+++ non-Karelian_denovo_assembly/quality_check/busco_stats.sh
@@ -13,15 +13,13 @@ Mypath=/mnt/tank/scratch/vshumakova/noncarel_assembler
 ## data
 DATA1=${Mypath}/masurca/CA/primary.genome.scf.fasta
 DATA2=${Mypath}/polish/pilon/noncarel.polished.fasta
-DATA3=${Mypath}/ragtag/scaf_output/chrs.noncarel.fasta
 DATA4=${Mypath}/ragtag/nucmer2/chrs.noncarel.fasta
 
 
 # busco
 #python --version
 
-#busco -i $DATA1 -m genome --offline --cpu 10 --lineage_dataset ${Mypath}/busco/embryophyta_odb10 -o masurca
-#busco -i $DATA2 -m genome --offline --cpu 10 --lineage_dataset ${Mypath}/busco/embryophyta_odb10 -o pilon
-#busco -i $DATA3 -m genome --offline --cpu 10 --lineage_dataset ${Mypath}/busco/embryophyta_odb10 -o ragtag
+busco -i $DATA1 -m genome --offline --cpu 10 --lineage_dataset ${Mypath}/busco/embryophyta_odb10 -o masurca
+busco -i $DATA2 -m genome --offline --cpu 10 --lineage_dataset ${Mypath}/busco/embryophyta_odb10 -o pilon
 busco -i $DATA4 -m genome --offline --cpu 10 --lineage_dataset ${Mypath}/busco/embryophyta_odb10 -o ragtag2
 #generate_plot.py -wd ${Mypath}/summaries
