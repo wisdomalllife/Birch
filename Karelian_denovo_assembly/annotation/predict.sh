276f46c085bcb4b139fca0d1142a3fc8c9dbbccb
wisdomalllife <wisdomalllife@gmail.com>
2025-04-13 18:27:54 +0300

    add anotation and envs

---
+++ Karelian_denovo_assembly/annotation/predict.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -i
+#
+#SBATCH --job-name=augustus
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=120G
+#SBATCH --time=48:00:00
+
+conda activate augustus
+## path
+Mypath=/mnt/tank/scratch/vshumakova/carel_assembler
+## data
+DATA=${Mypath}/masurca/ragtag/nucmer2/nucmer.fasta
+
+augustus --species=arabidopsis $DATA > karelian.gff
